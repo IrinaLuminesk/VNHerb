d7210bfddc910b14855fb923c1ce3a76046f82e3
Irina Luminesk <100247516+IrinaLuminesk@users.noreply.github.com>
2025-10-11 09:14:16 +0700

    Add more architectures

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50, ResNet50_Weights, densenet201, DenseNet201_Weights, vgg19, VGG19_Weights
 
 def build_model(model_type, num_classes):
         match model_type:
@@ -10,6 +10,31 @@ def build_model(model_type, num_classes):
                 in_features = model.fc.in_features
                 model.fc = nn.Linear(in_features, num_classes)
                 return model
+            case "DenseNet":
+                densenet_Weights = DenseNet201_Weights.DEFAULT
+                model = densenet201(weights=densenet_Weights)
+
+                in_features = model.classifier.in_features
+                model.classifier = nn.Linear(in_features, num_classes)
+
+                return model
+            case "VGG19":
+                vgg19_weights = VGG19_Weights.DEFAULT
+                model = vgg19(weights=vgg19_weights)
+
+                vgg19_classifier = list(model.classifier.children())[:6]
+                in_features = model.classifier[6]
+
+                model.classifier = nn.Sequential(
+                    *vgg19_classifier,
+                    nn.Linear(in_features, num_classes, bias=True)
+                )
+                return model
+            case "Swim":
+                return 1
+            case "MobileNet":
+                return 1
+            
 
 class Model(nn.Module):
     def __init__(self, num_classes, model_type, pretrained=True):
