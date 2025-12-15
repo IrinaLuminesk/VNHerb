ba18529ea9dc33d1c807d9e8706ea442d92118f8
Irina Luminesk <100247516+IrinaLuminesk@users.noreply.github.com>
2025-12-15 19:38:49 +0700

    Adding Inception model

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights,\
     densenet201, DenseNet201_Weights,\
-    vgg19, VGG19_Weights, \
+    vgg16, VGG16_Weights, \
     convnext_base, ConvNeXt_Base_Weights,\
     mobilenet_v2, MobileNet_V2_Weights, \
-    swin_v2_b, Swin_V2_B_Weights
+    swin_v2_b, Swin_V2_B_Weights, \
+    inception_v3, Inception_V3_Weights
 
 # def build_model(model_type: int, num_classes: int):
 #         match model_type:
@@ -114,30 +115,15 @@ class Model(nn.Module):
                 )
                 model.fc = fc
                 return model
-            case 2: #DenseNet201
-                densenet_Weights = DenseNet201_Weights.DEFAULT
-                model = densenet201(weights=densenet_Weights)
+            case 2: #VGG16
+                vgg16_weights = VGG16_Weights.DEFAULT
+                model = vgg16(weights=vgg16_weights)
 
-                in_features = model.classifier.in_features #1920
-                fc = nn.Sequential(
-                    nn.Linear(in_features, 1024),
-                    nn.BatchNorm1d(1024),
-                    nn.ReLU(),
-                    nn.Dropout(0.4),
-                    nn.Linear(1024, self.num_classes),
-                )
-                model.classifier = fc
-
-                return model
-            case 3: #VGG19
-                vgg19_weights = VGG19_Weights.DEFAULT
-                model = vgg19(weights=vgg19_weights)
-
-                vgg19_classifier = list(model.classifier.children())[:6]
+                vgg16_classifier = list(model.classifier.children())[:6]
                 in_features = model.classifier[6].in_features #4096
 
                 model.classifier = nn.Sequential(
-                    *vgg19_classifier,
+                    *vgg16_classifier,
                     nn.Linear(in_features, 2048, bias=True),
                     nn.BatchNorm1d(2048),
                     nn.ReLU(),
@@ -149,39 +135,68 @@ class Model(nn.Module):
                     nn.Linear(1024, self.num_classes)
                 )
                 return model
-            case 4: #convnext base
-                convnext_weight = ConvNeXt_Base_Weights.DEFAULT
-                model = convnext_base(weights= convnext_weight)
+            case 3: #Inception
+                inception_v3_weight = Inception_V3_Weights.DEFAULT
+                model = inception_v3(inception_v3_weight)
 
-                convnext_classifier = list(model.classifier.children())[:2]
-                in_features = model.classifier[2].in_features
-
-                model.classifier = nn.Sequential(
-                    *convnext_classifier,
-                    nn.Linear(in_features, self.num_classes, bias=True)
-                )
-                return model
-            case 5: #MobileNet
-                mobilenetv2_weights = MobileNet_V2_Weights.DEFAULT
-                model = mobilenet_v2(weights=mobilenetv2_weights)
-
-                in_features = model.classifier[1].in_features #1280
-
-                model.classifier = nn.Sequential(
-                    nn.Linear(in_features, 1024, bias=True),
+                in_features = model.fc.in_features #2048
+                fc = nn.Sequential(
+                    nn.Linear(in_features, 1024),
                     nn.BatchNorm1d(1024),
                     nn.ReLU(),
                     nn.Dropout(0.4),
-                    nn.Linear(1024, self.num_classes)
+                    nn.Linear(1024, self.num_classes),
                 )
+                model.fc = fc
                 return model
-            case 6: #Swin transform
-                swinv2Weight = Swin_V2_B_Weights.DEFAULT
-                model = swin_v2_b(weights=swinv2Weight)
+            # case 2: #DenseNet201
+            #     densenet_Weights = DenseNet201_Weights.DEFAULT
+            #     model = densenet201(weights=densenet_Weights)
 
-                in_features = model.head.in_features #1024
-                model.head = nn.Linear(in_features, self.num_classes, bias=True)
-                return model
+            #     in_features = model.classifier.in_features #1920
+            #     fc = nn.Sequential(
+            #         nn.Linear(in_features, 1024),
+            #         nn.BatchNorm1d(1024),
+            #         nn.ReLU(),
+            #         nn.Dropout(0.4),
+            #         nn.Linear(1024, self.num_classes),
+            #     )
+            #     model.classifier = fc
+
+            #     return model
+            # case 4: #convnext base
+            #     convnext_weight = ConvNeXt_Base_Weights.DEFAULT
+            #     model = convnext_base(weights= convnext_weight)
+
+            #     convnext_classifier = list(model.classifier.children())[:2]
+            #     in_features = model.classifier[2].in_features
+
+            #     model.classifier = nn.Sequential(
+            #         *convnext_classifier,
+            #         nn.Linear(in_features, self.num_classes, bias=True)
+            #     )
+            #     return model
+            # case 5: #MobileNet
+            #     mobilenetv2_weights = MobileNet_V2_Weights.DEFAULT
+            #     model = mobilenet_v2(weights=mobilenetv2_weights)
+
+            #     in_features = model.classifier[1].in_features #1280
+
+            #     model.classifier = nn.Sequential(
+            #         nn.Linear(in_features, 1024, bias=True),
+            #         nn.BatchNorm1d(1024),
+            #         nn.ReLU(),
+            #         nn.Dropout(0.4),
+            #         nn.Linear(1024, self.num_classes)
+            #     )
+            #     return model
+            # case 6: #Swin transform
+            #     swinv2Weight = Swin_V2_B_Weights.DEFAULT
+            #     model = swin_v2_b(weights=swinv2Weight)
+
+            #     in_features = model.head.in_features #1024
+            #     model.head = nn.Linear(in_features, self.num_classes, bias=True)
+            #     return model
     def forward(self, x):
         return self.model(x)
     
