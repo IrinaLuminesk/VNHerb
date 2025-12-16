291b2a1e8212f7235d3b9bbdc641c1f59bc2fc03
Irina Luminesk <100247516+IrinaLuminesk@users.noreply.github.com>
2025-12-16 22:46:35 +0700

    Changing Configuration

--- train.py
+++ train.py
@@ -92,8 +92,8 @@ def main():
     patience = config["TRAIN"]["TRAIN_PARA"]["PATIENCE"]
     epochs_no_improve = 0
     model_type = int(config["TRAIN"]["TRAIN_PARA"]["MODEL_TYPE"])
-    if model_type in [8,9]:
-        img_size = [224, 224]
+    # if model_type in [8,9]:
+    #     img_size = [224, 224]
     #Optional
     save_checkpoint = config["TRAIN"]["OPTIONAL"]["SAVE_CHECKPOINT"]
     save_best = config["TRAIN"]["OPTIONAL"]["SAVE_BEST"]
