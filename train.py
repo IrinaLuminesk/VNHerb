64f5442e605d4166ebf7439ea3ef12461b0377af
Irina Luminesk <100247516+IrinaLuminesk@users.noreply.github.com>
2026-01-27 13:07:18 +0700

    Fix error

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ def main():
     #Training parameters
     img_size = config["TRAIN"]["DATA"]["IMAGE_SIZE"]
     enabled_transform = config["TRAIN"]["TRANSFORM"]
-    enabled_batchwise_transform = config["AUG"]["ENABLED"]
+    enabled_batchwise_transform = config["TRAIN"]["AUG"]["ENABLED"]
     begin_epoch = config["TRAIN"]["TRAIN_PARA"]["BEGIN_EPOCH"] 
     end_epoch = config["TRAIN"]["TRAIN_PARA"]["END_EPOCH"]
     resume = config["TRAIN"]["TRAIN_PARA"]["RESUME"]
